72c539eddfe883faf8d941124062f5f71087335d
Gabo Martinez <gabmart02@gmail.com>
2018-04-10 14:45:33 -0400

    Code Review Doc

--- src/test/java/SortDemoTest.java
+++ src/test/java/SortDemoTest.java
@@ -78,16 +78,15 @@ class SortDemoTest {
     /**
      * SameNumber checks that duplicate numbers are removed
      */
-    // @Test
-    // public void SameNumber() {
-    //     data.initializeArray("5 5 64 4 4");  
-        
-    //     SortDemoData data2 = new SortDemoData();
-    //     data2.initializeArray("5 64 4");
-    //     assertTrue(data.myArray.length == 3);
-    //     assertTrue(data.myArray[0].key == data2.myArray[0].key );
-    //     assertTrue(data.myArray[1].key == data2.myArray[1].key );
-    //     assertTrue(data.myArray[2].key == data2.myArray[2].key );
-    // }
+     @Test
+     public void SameNumber() {
+         data.initializeArray("5 5 64 4 4");    
+         SortDemoData data2 = new SortDemoData();
+         data2.initializeArray("5 64 4");
+         assertTrue(data.myArray.length == 3);
+         assertTrue(data.myArray[0].key == data2.myArray[0].key );
+         assertTrue(data.myArray[1].key == data2.myArray[1].key );
+         assertTrue(data.myArray[2].key == data2.myArray[2].key );
+     }
     
 }
